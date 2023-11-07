cc1934159847cbf98d76a35e1f4f4e0762f1f4e3
RimmaBuzuluk <rimmysya2000@gmail.com>
2023-11-07 17:56:37 +0200

    check login

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -28,16 +28,24 @@ class UserController{
         return res.json({token})
     }
 
-    async login(req, res){
-        
+    async login(req, res, next){
+        const {email, password}=req.body
+        const user=await User.findOne({where:{email}})
+        if(!user){
+            return next(ApiError.internal('Користувач не зареєстрований'))
+        }
+        let comparePassword=bcrypt.compareSync(password, user.password)
+        if(!comparePassword){
+            return next(ApiError.internal('ВКАЗАН НЕВІРНИЙ ПАРОЛЬ'))
+        }
+
+        const token =generateJwt(user.id, user.email, user.role, user.name, user.surname, user.city);
+        return res.json({token})
     }
 
     async check(req, res, next){
-        const {id}=req.query
-        if(!id){
-         return   next(ApiError.badRequest('не знайден id'))
-        }
-        res.json(id)
+        const token =generateJwt(req.user.id, req.user.email, req.user.role, req.user.name, req.user.surname, req.user.city)
+        return res.json({token})
     }
 }
 
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,11 +1,11 @@
 const Router=require('express')
 const router=new Router()
-const UserController=require('../controllers/userControllers')
 const userControllers = require('../controllers/userControllers')
+const authMiddleware=require("../middleware/authMiddleware")
 
 router.post('/registration',userControllers.registration)
-router.get('/login', userControllers.login)
-router.get('/auth', userControllers.check )
+router.post('/login', userControllers.login)
+router.get('/auth',authMiddleware, userControllers.check )
 
 
 module.exports=router
